29943ce4f5e11838302448bc16049948d14b2ad7
Erik Lindgren <erik.lindgren@192.com>
2017-04-12 13:18:24 -0500

    Complete implementation of feature 1 - 5

---
+++ src/main/java/uk/co/lindgrens/stringcalculator/StringCalculator.java
@@ -0,0 +1,110 @@
+package uk.co.lindgrens.stringcalculator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringCalculator {
+    private static final String DEFAULT_DELIMITER = ",";
+
+    public int add(String numbers) {
+        if(numbers.isEmpty()) {
+            return 0;
+        }
+
+        return calculateSum(parseInput(numbers));
+    }
+
+    private List<String> parseInput(String numbers) {
+        String numberSeries = extractNumberSequence(numbers);
+        String delimiter = getDelimiter(numbers);
+
+        String sanitisedNumberSeries = sanitiseNumberSeries(delimiter, numberSeries);
+        return getNumbersAsListOfStrings(delimiter, sanitisedNumberSeries);
+    }
+
+
+    private String extractNumberSequence(String numbers) {
+        if(isDefaultDelimiter(numbers)) {
+            return numbers;
+        }
+
+        int startPos = numbers.indexOf("\n") + 1;
+        return numbers.substring(startPos, numbers.length());
+    }
+
+    private boolean isDefaultDelimiter(String numbers) {
+        return !numbers.startsWith("//");
+    }
+
+    private String getDelimiter(String numbers) {
+        Pattern pattern = Pattern.compile("//(.)");
+        Matcher matcher = pattern.matcher(numbers);
+
+        if(matcher.find()) {
+            return matcher.group(1);
+        }
+
+        return DEFAULT_DELIMITER;
+    }
+
+    private String sanitiseNumberSeries(String delimiter, String numberSeries) {
+        return numberSeries.replaceAll("\\n", delimiter);
+    }
+
+    private List<String> getNumbersAsListOfStrings(String delimiter, String numberSeries) {
+        return Arrays.asList(numberSeries.split(delimiter));
+    }
+
+   private int calculateSum(List<String> numbersList) {
+        int total = 0;
+        List<Integer> errors = new ArrayList<>();
+        for(String val : numbersList) {
+            int num = Integer.parseInt(val);
+            reportNegativeNumber(errors, num);
+            total = calculateNewTotal(total, num);
+        }
+
+        throwExceptionIfNegativeNumbersAreReported(errors);
+        return total;
+    }
+
+    private void reportNegativeNumber(List<Integer> errors, int num) {
+        if(num < 0) {
+            errors.add(num);
+        }
+    }
+
+    private int calculateNewTotal(int sum, int num) {
+        if(num > 0) {
+            sum +=num;
+        }
+
+        return sum;
+    }
+
+    private void throwExceptionIfNegativeNumbersAreReported(List<Integer> errors) {
+        if (errors.isEmpty()) {
+            return;
+        }
+
+        int pos = 0;
+        StringBuilder exceptionMessage = new StringBuilder();
+        for(int negativeNum : errors) {
+            writeExceptionMessage(errors.size(), pos, exceptionMessage, negativeNum);
+            pos++;
+        }
+
+        throw new IllegalArgumentException(exceptionMessage.toString());
+    }
+
+    private void writeExceptionMessage(int sizeOfList, int pos, StringBuilder exceptionMessage, int negativeNum) {
+        exceptionMessage.append(negativeNum);
+
+        if (pos < sizeOfList - 1) {
+            exceptionMessage.append(",");
+        }
+    }
+}
---
+++ src/test/groovy/StringCalculatorTest.groovy
@@ -0,0 +1,36 @@
+import spock.lang.Specification
+import spock.lang.Unroll
+import uk.co.lindgrens.stringcalculator.StringCalculator
+
+class StringCalculatorTest extends Specification {
+    StringCalculator stringCalculator
+
+    def setup() {
+        stringCalculator = new StringCalculator()
+    }
+    @Unroll
+    def "should calculate the sum of numbers"() {
+        expect:
+        stringCalculator.add(NUMBERS) == SUM
+
+        where:
+        NUMBERS         | SUM
+        ''              | 0
+        '1'             | 1
+        '1,2'           | 3
+        '1\n2,3'        | 6
+        '//;\n1;2'      | 3
+    }
+
+    def "should throw exception for negative numbers"() {
+        given:
+        String negativeNumbers = '-1,-2,-3'
+
+        when:
+        stringCalculator.add(negativeNumbers)
+
+        then:
+        Exception exception = thrown(IllegalArgumentException)
+        exception.message == '-1,-2,-3'
+    }
+}
